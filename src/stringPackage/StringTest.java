1cc3aada6e6f38a45c9ef8fba865601cb1062399
1760281813 <1760281813@qq.com>
2019-12-08 12:38:41 +0800

    Initial commit

---
+++ src/stringPackage/StringTest.java
@@ -0,0 +1,10 @@
+package stringPackage;
+
+public class StringTest {
+    public static void main(String[] args) {
+        double db = 123.123;
+        //保留小数位数
+        System.out.println(String.format("%.2f",db));
+
+    }
+}
